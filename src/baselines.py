d5e7e6369ddb51c072e72f93ec00c33477d214f4
Francois Roewer-Despres <francois-rd@users.noreply.github.com>
2019-04-19 02:34:14 -0400

    Add extra output to MLE baselines.

--- src/baselines.py
+++ src/baselines.py
@@ -10,23 +10,28 @@
 # - No smoothing for this baseline
 # - Set N in {1, 2, 3, median num phonemes/word} and see which is best.
 from src.align_phonemes import ALIGNMENT_OPTS, OUTPUT_OPTS, PHONEME_OUT
+from sklearn.metrics import accuracy_score, precision_recall_curve, auc
+from sklearn.metrics import confusion_matrix, roc_curve, roc_auc_score
 from src.utils import discrete_value_error_message as error_msg
 from src.evaluation import evaluate_model
 from src.data_loader import DataLoader
 import collections
 import pickle
 import time
+import os
 
 MLE_OUT_DIR = "../output/MLE/"
 
 
-def mle_baseline(data_dir, out_dir, n, alignment, output, dataset, cached=True):
+def mle_baseline(data_dir, out_dir, n, weight, alignment, output, dataset,
+                 cached=True):
     """
     Trains and evaluate an MLE baseline for the given parameters.
 
     :param data_dir: the directory from which to load the files
     :param out_dir: the directory to which to save the resulting files
     :param n: the n in n-gram
+    :param weight: how much to weight mismatches over matches
     :param alignment: set to 'hypothesis'
     :param output: one of ['full', 'abridged', 'binary']
     :param dataset: one of ['val', 'test']
@@ -40,35 +45,43 @@ def mle_baseline(data_dir, out_dir, n, alignment, output, dataset, cached=True):
     if dataset not in ['val', 'test']:
         raise ValueError(error_msg('dataset', ['val', 'test']))
     # Training.
-    file = out_dir + "mle_" + alignment + "_" + output + ".json"
+    file = out_dir + "mle_{}_{}_{}_{}".format(n, weight, alignment, output)
     try:
         if cached:  # If should used cached model and cached model exists.
-            with open(file, 'rb') as f:
+            with open(file + ".json", 'rb') as f:
                 mle = pickle.load(f)  # Load it.
         else:  # Otherwise, train it from scratch and cache it for later.
             raise FileNotFoundError
     except FileNotFoundError:
-        mle = _train_mle_model(data_dir, n, alignment, output)
-        with open(file, 'wb') as f:
+        mle = _train_mle_model(data_dir, n, weight, alignment, output)
+        with open(file + ".json", 'wb') as f:
             pickle.dump(mle, f)
     # Use the model to make predictions, and then evaluate the model.
     print("Loading test data...")
     dl = DataLoader(data_dir, PHONEME_OUT, alignment, output, dataset, 100000)
     print("Done.")
     refs, hyps = dl.__iter__().__next__()
-    pred_hyps = _predict_mle_model(mle, refs, n)
+    pred_hyps, pred_probs = _predict_mle_model(mle, refs, n)
+    for h, hp in zip(hyps, pred_hyps):
+        if len(h) != len(hp):
+            print(h)
+            print(hp)
+    if output == 'binary':
+        _save_results_for_binary(hyps, pred_hyps, pred_probs, weight, file)
     print("Evaluating...")
     start = time.time()
     evaluate_model(refs, hyps, pred_hyps, out_dir, alignment, output)
+    os.rename(out_dir + alignment + "_" + output + ".txt", file + ".txt")
     print("Done. Elapsed time: {:.2f} seconds".format(time.time() - start))
 
 
-def _train_mle_model(data_dir, n, alignment, output):
+def _train_mle_model(data_dir, n, weight, alignment, output):
     """
     Trains an MLE model with the given parameters.
 
     :param data_dir: the directory from which to load the files
     :param n: the n in n-gram
+    :param weight: how much to weight mismatches over matches
     :param alignment: set to 'hypothesis'
     :param output: one of ['full', 'abridged', 'binary']
     :return: dict(tuple -> dict(int -> int)) (a representation of the MLE model)
@@ -89,7 +102,12 @@ def _train_mle_model(data_dir, n, alignment, output):
     refs, hyps = _prepend(refs, n_minus_1), _prepend(hyps, n_minus_1)
     for ref, hyp in zip(refs, hyps):
         for i in range(n_minus_1, len(ref)):
-            mle[tuple(ref[i - n_minus_1:i + 1])][hyp[i]] += 1
+            value = hyp[i]
+            if output == 'full':
+                same = ref[i] == value
+            else:
+                same = value == 0
+            mle[tuple(ref[i - n_minus_1:i + 1])][value] += 1 if same else weight
     # Normalizing.
     for value_dict in mle.values():
         total = sum([count for count in value_dict.values()])
@@ -120,9 +138,11 @@ def _predict_mle_model(mle, refs, n):
     :param mle: a trained MLE model of the form dict(tuple -> dict(int -> int))
     :param refs: inputs for prediction (list of lists of integers)
     :param n: the n in n-gram (must match the MLE model)
-    :return: a list of lists of integers representing the predictions
+    :return: a list of lists of integers representing the predictions, and a
+        list of prediction probabilities (only used for output == 'binary')
     """
     pred, n_minus_1, count, total = [], n - 1, 0, 0
+    pred_probs = []
     print("Predicting...")
     for ref in _prepend(refs, n_minus_1):
         hyp = []
@@ -132,13 +152,63 @@ def _predict_mle_model(mle, refs, n):
                 value_dict = mle[tuple(ref[i - n_minus_1:i + 1])]
                 hyp.append(max(value_dict.keys(), key=lambda k: value_dict[k]))
             except KeyError:
-                # Naively skip the output if the context was never seen.
                 # TODO: histogram of which n-grams causes these key errors.
                 hyp.append(-1)
                 count += 1
+            try:
+                pred_probs.append(mle[tuple(ref[i - n_minus_1:i + 1])][1])
+            except KeyError:
+                pred_probs.append(0)
         pred.append(hyp)
-    print("Done. {} key errors of {} total key searches".format(count, total))
-    return pred
+    print("Done. {} key errors of {} total key searches.".format(count, total))
+    return pred, pred_probs
+
+
+def _save_results_for_binary(hyps, pred_hyps, pred_probs, weight, file):
+    """
+    Saves a bunch of extra results in the case where output == 'binary'.
+
+    :param hyps: the true hypotheses (list of lists of 0/1)
+    :param pred_hyps: the predicted hypotheses (list of lists of 0/1)
+    :param pred_probs: the probability that a prediction is 1 (list of float)
+    :param weight: the weight used to train the MLE model
+    :param file: the root file path for saving the resulting files
+    """
+    import matplotlib.pyplot as plt
+
+    with open(file + "_extra_output.txt", 'w') as f:
+        bin_hyps = [h for hyp in hyps for h in hyp]
+        bin_pred_hyps = [max(0, h) for hyp in pred_hyps for h in hyp]
+        tn, fp, fn, tp = confusion_matrix(bin_hyps, bin_pred_hyps).ravel()
+        print("TN,{}\nFP,{}\nFN,{}\nTP,{}".format(tn, fp, fn, tp), file=f)
+        print("ACC," + str(accuracy_score(bin_hyps, bin_pred_hyps)), file=f)
+        fpr, sens, thresh = roc_curve(bin_hyps, pred_probs)
+        print("ROC_FPR," + ','.join([str(f) for f in fpr]), file=f)
+        print("ROC_SENS," + ','.join([str(s) for s in sens]), file=f)
+        print("ROC_THRESHOLD," + ','.join([str(t) for t in thresh]), file=f)
+        plt.figure()
+        plt.plot([0, 1], [0, 1], linestyle='--')
+        plt.plot(fpr, sens, marker='.')
+        plt.title("ROC for MLE model with weight " + str(weight))
+        plt.xlabel("False Positive Rate")
+        plt.ylabel("Sensitivity")
+        plt.savefig(file + "_ROC.pdf")
+        print("ROC_AUC," + str(roc_auc_score(bin_hyps, pred_probs)), file=f)
+        prec, rec, thresh = precision_recall_curve(bin_hyps, pred_probs)
+        print("PRC_PREC," + ','.join([str(p) for p in prec]), file=f)
+        print("PRC_REC," + ','.join([str(r) for r in rec]), file=f)
+        print("PRC_THRESHOLD," + ','.join([str(t) for t in thresh]), file=f)
+        plt.figure()
+        plt.plot([0, 1], [0.5, 0.5], linestyle='--')
+        plt.plot(rec, prec, marker='.')
+        plt.title("PRC for MLE model with weight " + str(weight))
+        plt.xlabel("Recall")
+        plt.ylabel("Precision")
+        plt.savefig(file + "_PRC.pdf")
+        print("PRC_AUC," + str(auc(rec, prec)), file=f)
+    with open(file + "_predictions.txt", 'w') as f:
+        for pred in pred_hyps:
+            print(' '.join([str(p) for p in pred]), file=f)
 
 
 if __name__ == '__main__':
@@ -146,7 +216,8 @@ if __name__ == '__main__':
 
     for align in ALIGNMENT_OPTS:
         for out in OUTPUT_OPTS:
-            if align != 'none' and out != 'scalar':
-                print(align, out)
-                mle_baseline(DATA_DIR, MLE_OUT_DIR, 3, align, out, 'val',
-                             cached=False)
+            if align != 'none' and out == 'binary':
+                for w in [1, 5, 10, 20]:
+                    print(align, out, w)
+                    mle_baseline(DATA_DIR, MLE_OUT_DIR, 3, w, align, out, 'val',
+                                 cached=False)
